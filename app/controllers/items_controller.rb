597cd3eaf936e39b44016f15cf0f23e1cf750b48
Ryotak41 <r_tak23@yahoo.co.jp>
2022-02-12 21:20:42 +0900

    商品情報編集機能 commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :current_user_check, only:[:edit, :update]
 
   def index
     @items = Item.order('created_at DESC')
@@ -21,6 +22,21 @@ class ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
   end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    if @item.update(item_params)
+      redirect_to action: :show
+    else
+      render :edit
+    end
+  end
+  
 end
 
 private  # private以下の記述はすべてプライベートメソッドになる
@@ -29,4 +45,11 @@ def item_params  # プライベートメソッド
   params.require(:item).permit(:image, :product, :description, :category_id, :situation_id, :delivery_charge_id, :prefecture_id,
                                :shipping_day_id, :price)
         .merge(user_id: current_user.id)
+end
+
+def current_user_check
+  item = Item.find(params[:id])
+  if item.user_id != current_user.id
+    redirect_to action: :index
+  end
 end
