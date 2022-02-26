7a40f521873668f6f3c20aff337b8fa21fdfd420
Ryotak41 <r_tak23@yahoo.co.jp>
2022-02-26 18:50:08 +0900

    Basic認証 commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
@@ -8,4 +9,11 @@ class ApplicationController < ActionController::Base
                                       keys: [:nickname, :email, :encrypted_password, :last_name, :name, :last_name_kana,
                                              :name_kana, :birthday])
   end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]  # 環境変数を読み込む記述
+    end
+  end
+
 end
